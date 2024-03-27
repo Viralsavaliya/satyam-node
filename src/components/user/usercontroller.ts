1a23223563ca1d3d0d6cf78b90ff3994a90a1b80
priyankambliya <priyankambliya18@gmail.com>
2024-03-27 22:04:43 +0530

    changes for userController.ts

--- src/components/user/usercontroller.ts
+++ src/components/user/usercontroller.ts
@@ -35,17 +35,17 @@ const AppleAuth = require("apple-auth");
 const log4js = require("log4js");
 const logger = log4js.getLogger();
 
-const appleAuth = new AppleAuth(
-    {
-        "client_id": "com.whitetailtactical",
-        "team_id": "MP4BRQ22MX",
-        "key_id": "KN5CUTPN63",
-        "redirect_uri": "https://white.elaunchinfotech.in/auth/apple",
-        "scope": "name email"
-    },
-    fs.readFileSync("./config/AuthKey.p8").toString(), //read the key file
-    "text"
-);
+// const appleAuth = new AppleAuth(
+//     {
+//         "client_id": "com.whitetailtactical",
+//         "team_id": "MP4BRQ22MX",
+//         "key_id": "KN5CUTPN63",
+//         "redirect_uri": "https://white.elaunchinfotech.in/auth/apple",
+//         "scope": "name email"
+//     },
+//     fs.readFileSync("./config/AuthKey.p8").toString(), //read the key file
+//     "text"
+// );
 
 async function guestLogin(req: Request, res: Response) {
     try {
