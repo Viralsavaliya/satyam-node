19730877e793632295d088c146fd935306114372
priyankambliya <priyankambliya18@gmail.com>
2024-03-27 22:26:20 +0530

    delete file..

--- build/utils/firebaseConfig.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-// var notification = require("firebase-admin");
-// var serviceAccount = require("./firebaseToken.json");
-// notification.initializeApp({
-//   credential: notification.credential.cert(serviceAccount),
-// })
-// export default notification
--- build/utils/redisHelper.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-// const Redis = require("ioredis");
-// const config = require('config');
-// const redisClient = new Redis(config.get("REDIS_PORT"), config.get("REDIS_HOST"));
-// // export default redisClient;
